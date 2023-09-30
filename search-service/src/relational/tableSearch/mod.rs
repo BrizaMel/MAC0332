44f13e11f1a9750d457b8948f716ecd3326ba519
ArthurPilone <art.pilone@gmail.com>
2023-09-30 17:50:15 -0300

    feat: TableSearch pathTo method shows path between tables
    Signed-off-by: ArthurPilone <art.pilone@gmail.com>

--- search-service/src/relational/tableSearch/mod.rs
+++ search-service/src/relational/tableSearch/mod.rs
@@ -3,24 +3,27 @@
 // of operations involving fields from different tables of a 
 // given relational database.
 
+use std::collections::HashMap;
+
 use dict::{Dict, DictIface};
-use petgraph::graph::{Graph,NodeIndex}; // Graph used to represent foreign key links
+use petgraph::{graph::{Graph,NodeIndex}, Undirected}; // Graph used to represent foreign key links
 // use union_find::QuickFindUf; // Union Find used to quickly determine if an operation is possible
 
 use crate::relational::general::{ForeignKey, Table};
 
 use petgraph::dot::{Dot, Config}; //Used for debugging graphs
+use petgraph::algo::dijkstra;
 
 pub struct TableSearch {
 	indexes_dict: Dict::<NodeIndex>,
-	table_graph: Graph::<String, String>,
+	table_graph: Graph::<String, String, Undirected>,
 	// Is the UF really necessary?
 }
 
 impl TableSearch {
 	pub fn new(tables: &Vec<Table>, foreign_keys: &Vec<ForeignKey>) -> Self {
 
-		let mut this_graph = Graph::<String, String>::new();
+		let mut this_graph = Graph::<String, String, Undirected>::new_undirected();
 		let mut this_dict  = Dict::<NodeIndex>::new();
 
 		for table in tables{
@@ -50,9 +53,39 @@ impl TableSearch {
 		}
 	}
 
+	pub fn pathTo(&self, origin: String, destiny: String) -> Vec<&String>{
+		let origin_index = self.indexes_dict.get(&origin).unwrap();
+		let destiny_index = self.indexes_dict.get(&destiny).unwrap();
+
+		let mut nodes: Vec<&String> = Vec::new();
+
+		let result_path = dijkstra(&self.table_graph,
+			*origin_index,
+			Some(*destiny_index),
+			|_| 1
+		);
+		
+		let mut oredered_nodes: Vec<_> = result_path.iter().collect();
+		oredered_nodes.sort_by(|a, b| a.1.cmp(b.1));
+
+		for entry in oredered_nodes{
+			// nodes.set
+			let table_identifier = self.table_graph.node_weight(*entry.0).unwrap();
+			nodes.push(table_identifier);
+		}
+
+		if nodes[nodes.len()-1] != &destiny{
+			nodes = Vec::new();
+		}
+
+		nodes
+	}
+
+
 }
 
-#[cfg(test)]mod tests {
+#[cfg(test)]
+mod tests {
     // Note this useful idiom: importing names from outer (for mod tests) scope.
     use super::*;
 
@@ -82,6 +115,86 @@ impl TableSearch {
 					"e".to_string(), "C".to_string(), 
 					"D".to_string(), "f".to_string())
 			])); 
+	}
+
+	#[test]
+	fn test_no_path(){
+		let ts = TableSearch::new(&Vec::from([
+			Table::new("A".to_string(),"B".to_string(),Vec::new(),Vec::new()),
+			Table::new("C".to_string(),"D".to_string(),Vec::new(),
+			Vec::new()),
+			Table::new("AA".to_string(),"BB".to_string(),Vec::new(),Vec::new()),
+			// Table::new("CA".to_string(),"DA".to_string(),Vec::new(),
+			// Vec::new())
+			]),
+			&Vec::from([
+				ForeignKey::new("A".to_string(), "B".to_string(), 
+					"e".to_string(), "C".to_string(), 
+					"D".to_string(), "f".to_string()),
+			])); 
+		let res = ts.pathTo("A.B".to_string(), "AA.BB".to_string());
+		let expected: Vec<&String> = Vec::from([]);
+		assert_eq!(res,expected);
+	}
+
+	#[test]
+	fn test_ordered_edges(){
+		let ts = TableSearch::new(&Vec::from([
+			Table::new("A".to_string(),"B".to_string(),Vec::new(),Vec::new()),
+			Table::new("C".to_string(),"D".to_string(),Vec::new(),
+			Vec::new()),
+			Table::new("AA".to_string(),"BB".to_string(),Vec::new(),Vec::new()),
+			// Table::new("CA".to_string(),"DA".to_string(),Vec::new(),
+			// Vec::new())
+			]),
+			&Vec::from([
+				ForeignKey::new("A".to_string(), "B".to_string(), 
+					"e".to_string(), "C".to_string(), 
+					"D".to_string(), "f".to_string()),
+				ForeignKey::new("C".to_string(), "D".to_string(), 
+					"g".to_string(), "AA".to_string(), 
+					"BB".to_string(), "h".to_string())
+			])); 
+		let res = ts.pathTo("A.B".to_string(), "AA.BB".to_string());
+		let node1 = "A.B".to_string();
+		let node2 = "C.D".to_string();
+		let node3 = "AA.BB".to_string();
+		let expected: Vec<&String> = Vec::from([
+			&node1,
+			&node2,
+			&node3
+		]);
+		assert_eq!(res,expected);
+	}
+
+	#[test]
+	fn test_inverted_edges(){
+		let ts = TableSearch::new(&Vec::from([
+			Table::new("A".to_string(),"B".to_string(),Vec::new(),Vec::new()),
+			Table::new("C".to_string(),"D".to_string(),Vec::new(),
+			Vec::new()),
+			Table::new("AA".to_string(),"BB".to_string(),Vec::new(),Vec::new()),
+			// Table::new("CA".to_string(),"DA".to_string(),Vec::new(),
+			// Vec::new())
+			]),
+			&Vec::from([
+				ForeignKey::new("AA".to_string(), "BB".to_string(), 
+					"g".to_string(), "C".to_string(), 
+					"D".to_string(), "h".to_string()),
+				ForeignKey::new("A".to_string(), "B".to_string(), 
+					"e".to_string(), "C".to_string(), 
+					"D".to_string(), "f".to_string()),
+			])); 
+		let res = ts.pathTo("A.B".to_string(), "AA.BB".to_string());
+		let node1 = "A.B".to_string();
+		let node2 = "C.D".to_string();
+		let node3 = "AA.BB".to_string();
+		let expected: Vec<&String> = Vec::from([
+			&node1,
+			&node2,
+			&node3
+		]);
+		assert_eq!(res,expected);
 	}
 
 }

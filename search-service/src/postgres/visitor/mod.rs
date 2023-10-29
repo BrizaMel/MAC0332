cd1b65690825c30e400586cdffc5ad0296d33403
ArthurPilone <art.pilone@gmail.com>
2023-10-29 11:08:33 -0300

    feat: Refactor architecture to pass TableSearch instance on PostgresVisitor
    Signed-off-by: ArthurPilone <art.pilone@gmail.com>

--- search-service/src/postgres/visitor/mod.rs
+++ search-service/src/postgres/visitor/mod.rs
@@ -1,3 +1,5 @@
+use crate::relational::general::{ForeignKey, Table};
+use crate::relational::tableSearch::TableSearch;
 use crate::traits::Visitor;
 
 use crate::query_representation::intermediary::Command;
@@ -8,12 +10,23 @@ use anyhow::Error;
 
 mod tests;
 
-#[derive(Default, Debug)]
-pub struct PostgresVisitor;
+#[derive(Default)]
+pub struct PostgresVisitor {
+    //TableSearch struct with information on the db's tables
+    table_search: TableSearch,
+}
+
+impl PostgresVisitor {
+    pub fn new(tables: &Vec<Table>, foreign_keys: &Vec<ForeignKey>) -> Self {
+        Self {
+            table_search: TableSearch::new(tables, foreign_keys),
+        }
+    }
+}
 
 impl Visitor for PostgresVisitor {
     fn visit_command(&self, projection: Vec<String>, command: &Command) -> Result<String, Error> {
-        let query = command_to_query(projection, &command)?;
+        let query = command_to_query(&projection, command, &self.table_search)?;
 
         Ok(query)
     }
--- search-service/src/postgres/visitor/tests.rs
+++ search-service/src/postgres/visitor/tests.rs
@@ -1,6 +1,7 @@
 #[cfg(test)]
 mod tests {
 
+    use crate::relational::general::{ForeignKey, Table};
     use std::sync::Arc;
 
     use crate::query_representation::intermediary::Command;
@@ -25,14 +26,15 @@ mod tests {
         let cc_return = Command::CompositeCommand(composite_command)
             .accept(vec!["projection".to_string()], Arc::new(PostgresVisitor))?;
 
-        assert_eq!(
-            sc_return,
-            "Command to query not implemented yet".to_string()
-        );
-        assert_eq!(
-            cc_return,
-            "Command to query not implemented yet".to_string()
-        );
+        //TODO: Pass correct lists of Tables and ForeignKeys to visitor
+        let tables: Vec<Table> = Vec::from([]);
+        let fks: Vec<ForeignKey> = Vec::from([]);
+        let postgres_visitor = PostgresVisitor::new(&tables, &fks);
+
+        let sc_return = Command::SimpleCommand(simple_command)
+            .accept(vec!["projection".to_string()], &postgres_visitor)?;
+        let cc_return = Command::CompositeCommand(composite_command)
+            .accept(vec!["projection".to_string()], &postgres_visitor)?;
 
         Ok(())
     }

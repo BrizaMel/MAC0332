a9fabead3c3ddd0e232cfa6e7cd2da8bbbd14223
Lorenzo Bertin Salvador <lorenzobs@usp.br>
2023-12-05 00:48:50 -0300

    fix: Get tableSearch and schema info at each request.
    If the database changed since the server initialization, the response will always keep
    up with the db schema.
    
    Signed-off-by: Lorenzo Bertin Salvador <lorenzobs@usp.br>

--- search-service/src/manager/mod.rs
+++ search-service/src/manager/mod.rs
@@ -33,32 +33,17 @@ pub enum ManagerError {
 #[derive(Clone)]
 pub struct SearchServiceManager {
     pub storage: Arc<dyn SearchServiceStorage>,
-    pub db_schema: DbSchema,
-    pub table_search: TableSearch
 }
 
 impl SearchServiceManager {
     pub async fn new(storage: Arc<dyn SearchServiceStorage>) -> Self {
-
-        let db_schema = storage.get_db_schema_info().await.expect("Error getting schema info");
-
-        let tables_search_info: Vec<TableSearchInfo> = db_schema.tables
-            .clone()
-            .into_iter()
-            .map(TableSearchInfo::from)
-            .collect();
-
-        let table_search = TableSearch::new(tables_search_info, db_schema.foreign_keys.clone());
-
-        Self {
-            storage,
-            db_schema,
-            table_search
-        }
+        Self { storage }
     }
 
     pub async fn get_filter_properties(&self) -> Result<Properties, ManagerError> {
-        Ok(get_filter_properties(&self.db_schema, &self.table_search, &self.storage).await?)
+        let db_schema = self.storage.get_db_schema_info().await?;
+        let table_search = self.get_table_search(&db_schema).await?;
+        Ok(get_filter_properties(&db_schema, &table_search, &self.storage).await?)
     }
 
     pub async fn search(
@@ -78,12 +63,28 @@ impl SearchServiceManager {
         let command =
             initial_to_command(filters).map_err(|e| ManagerError::ParseError(e.to_string()))?;
 
-        let visitor = DatabaseVisitor::new(self.table_search.clone());
+        let table_search = self.get_table_search(
+            &self.storage.get_db_schema_info().await?
+        ).await?;
+
+        let visitor = DatabaseVisitor::new(table_search);
 
         let query = command
             .accept(projection, Arc::new(visitor))
             .map_err(|e| ManagerError::QueryBuildError(e.to_string()))?;
 
         Ok(self.storage.execute(query).await?)
+    }
+
+    async fn get_table_search(&self, db_schema: &DbSchema) -> Result<TableSearch, ManagerError> {
+        let tables_search_info: Vec<TableSearchInfo> = db_schema.tables
+            .clone()
+            .into_iter()
+            .map(TableSearchInfo::from)
+            .collect();
+            
+        let table_search = TableSearch::new(tables_search_info, db_schema.foreign_keys.clone());
+
+        Ok(table_search)
     }
 }

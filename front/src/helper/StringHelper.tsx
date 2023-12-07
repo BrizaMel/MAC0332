11509db02ec3d5f90c100d531873f2345dd723f1
Lorenzo Bertin Salvador <lorenzobs@usp.br>
2023-12-06 23:35:27 -0300

    fix: Fix communication with the initial parser
    Signed-off-by: Lorenzo Bertin Salvador <lorenzobs@usp.br>

--- front/src/helper/StringHelper.tsx
+++ front/src/helper/StringHelper.tsx
@@ -18,18 +18,47 @@ export function generateValidProjection(fields: string[]): string[] {
 export function generateStringFromQueryArray(
   query: QueryModelExport[]
 ): string {
-  var base = "(";
+  var base = "";
   query.forEach((q) => {
     base += `${convertReadableStringToPath(q.selectedAttribute)} ${
-      q.selectedOperator
+      translateOperator(q.selectedOperator)
     } ${q.selectedValue} ${
       q.subqueries != undefined && q.subqueries.length > 0
         ? q.selectedLogicalSubquerie
         : ""
-    } ${q.subqueries ? generateStringFromQueryArray(q.subqueries) : ""}  ${
+    } ${q.subqueries ? `(${generateStringFromQueryArray(q.subqueries)})` : ""}  ${
       q.selectedLogical ?? ""
     } `;
   });
-  base += " )";
-  return base.replace(/  +/g, " ");
+  return base.replace(/  +/g, " ").trim();
+}
+
+function translateOperator(operator: string): string{
+  let back_operator;
+
+  switch(operator) {
+  case "EqualTo":
+    back_operator = "eq";
+    break;
+  case "GreaterThan":
+    back_operator = "gt";
+    break;
+  case "LessThan":
+    back_operator = "lt";
+    break;
+  case "GreaterThanOrEqualTo":
+    back_operator = "ge";
+    break;
+  case "LessThanOrEqualTo":
+    back_operator = "le";
+    break;
+  case "NotEqualTo":
+    back_operator = "ne";
+    break;
+  default:
+    back_operator = "UNKOWN";
+  }
+
+  return back_operator;
+
 }
